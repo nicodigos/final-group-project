1f66e7ffe22eed15404ac4877340418b632c7474
Vishwas Singhi <singhivishwas2001@gmail.com>
2024-08-09 18:48:24 -0400

    Crud opperations

---
+++ app/crud.py
@@ -0,0 +1,32 @@
+# crud.py
+from sqlalchemy.orm import Session
+from . import models, schemas
+
+def create_transaction(db: Session, transaction: schemas.TransactionCreate):
+    db_transaction = models.Transaction(**transaction.dict())
+    db.add(db_transaction)
+    db.commit()
+    db.refresh(db_transaction)
+    return db_transaction
+
+def get_transactions(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(models.Transaction).offset(skip).limit(limit).all()
+
+def get_transaction_by_id(db: Session, transaction_id: int):
+    return db.query(models.Transaction).filter(models.Transaction.id == transaction_id).first()
+
+def update_transaction(db: Session, transaction_id: int, transaction: schemas.TransactionCreate):
+    db_transaction = db.query(models.Transaction).filter(models.Transaction.id == transaction_id).first()
+    if db_transaction:
+        for key, value in transaction.dict().items():
+            setattr(db_transaction, key, value)
+        db.commit()
+        db.refresh(db_transaction)
+    return db_transaction
+
+def delete_transaction(db: Session, transaction_id: int):
+    db_transaction = db.query(models.Transaction).filter(models.Transaction.id == transaction_id).first()
+    if db_transaction:
+        db.delete(db_transaction)
+        db.commit()
+    return db_transaction
